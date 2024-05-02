3382aa7741619223db2d3018c494671148f84656
2132112313 <2131>
2024-05-02 05:43:32 +0300

    Initial Commit

---
+++ LoginTestApp/LoginTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTestAppApp.swift
+//  LoginTestApp
+//
+//  Created by Федор Шашков on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
